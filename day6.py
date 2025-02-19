f90b937e7bf3747747308775f38d39f916b544dc
tsc4az <117491334+tsc4az@users.noreply.github.com>
2025-02-19 14:54:23 -0700

    day 6

---
+++ day6.py
@@ -0,0 +1,11 @@
+def Hello ():
+    print ("Hello")
+
+Hello()
+
+def get_user_name():
+    name = input("What is your name? ")
+    print("Hello, " + name)
+    #inside the function
+
+get_user_name()
